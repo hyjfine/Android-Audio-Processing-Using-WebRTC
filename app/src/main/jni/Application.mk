53c88f1fadcd535198294e1349ae5faaf1a806be
heyongjian <hyjfine@gmail.com>
2020-06-03 11:45:34 +0800

    update

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,6 +1,14 @@
 APP_ABI := armeabi-v7a arm64-v8a
-APP_STL := gnustl_static
 #APP_PLATFORM := android-19
 #APP_CPPFLAGS += -std=c++11
+
+#APP_STL := gnustl_static
+#APP_CPPFLAGS := -fno-rtti -fexceptions
+#APP_CPPFLAGS := -latomic
+
+APP_STL := c++_shared
 APP_CPPFLAGS := -fno-rtti -fexceptions
 APP_CPPFLAGS := -latomic
+#APP_CPPFLAGS := -frtti -std=c++11
+#APP_SHORT_COMMANDS := true
+#LOCAL_LDFLAGS += -fuse-ld=bfd
